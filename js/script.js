67aa676f0e84addc35ae116fb23ea38a382d847b
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-03-01 17:38:26 +0100

    completed

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let food = [...document.querySelectorAll(".food")]
+
+
+i = 0;
+
+setInterval(() => {
+    i++
+    let icon = document.querySelector(".change")
+    icon.classList.remove("change")
+
+    if (i >= food.length) {
+        food[0].classList.add('change')
+        i = 0
+
+    } else {
+        icon.nextElementSibling.classList.add("change")
+    }
+
+}, 3000);
