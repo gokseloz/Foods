c742d3d921291984ec63f68e774ba28081a94aff
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-03-05 16:14:21 +0100

    completed

--- js/script.js
+++ js/script.js
@@ -3,17 +3,16 @@ let food = [...document.querySelectorAll(".food")]
 
 i = 0;
 
-// setInterval(() => {
-//     i++
-//     let icon = document.querySelector(".change")
-//     icon.classList.remove("change")
+setInterval(() => {
+    i++
+    let icon = document.querySelector(".change")
+    icon.classList.remove("change")
 
-//     if (i >= food.length) {
-//         food[0].classList.add('change')
-//         i = 0
+    if (i >= food.length) {
+        food[0].classList.add('change')
+        i = 0
 
-//     } else {
-//         icon.nextElementSibling.classList.add("change")
-//     }
-
-// }, 3000);
+    } else {
+        icon.nextElementSibling.classList.add("change")
+    }
+}, 3000);
