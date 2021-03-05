4316aae715a1b63fdaa670abeef3ca360cb839dc
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-03-05 17:25:57 +0100

    nav added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let food = [...document.querySelectorAll(".food")]
 let menuBtn = document.querySelector(".menu-btn")
 let nav = document.querySelector(".nav")
+let navLink = [...document.querySelectorAll(".nav-link")]
 
 i = 0;
 
@@ -21,4 +22,11 @@ setInterval(() => {
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("is-active")
     nav.classList.toggle("is-active")
-} )
+    // setTimeout(() => {
+    //     for (i = 0; i < navLink.length; i++) {
+    //         navLink[i].style.cssText = `opacity:1; transition: opacity .2s ${i/10}s`
+    //     }
+    // }, 600);
+    
+
+})
