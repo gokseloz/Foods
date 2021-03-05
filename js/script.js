6f63850551b64045b60f018a21a3bcd3eaf1b72d
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-03-05 18:39:34 +0100

    mobile v.  completed

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-let food = [...document.querySelectorAll(".food")]
+let foodIcons = [...document.querySelectorAll(".food")]
 let menuBtn = document.querySelector(".menu-btn")
 let nav = document.querySelector(".nav")
-let navLink = [...document.querySelectorAll(".nav-link")]
+
 
 i = 0;
-
 setInterval(() => {
     i++
     let icon = document.querySelector(".change")
     icon.classList.remove("change")
 
-    if (i >= food.length) {
-        food[0].classList.add('change')
+    if (i >= foodIcons.length) {
+        foodIcons[0].classList.add('change')
         i = 0
 
     } else {
@@ -22,11 +21,5 @@ setInterval(() => {
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("is-active")
     nav.classList.toggle("is-active")
-    // setTimeout(() => {
-    //     for (i = 0; i < navLink.length; i++) {
-    //         navLink[i].style.cssText = `opacity:1; transition: opacity .2s ${i/10}s`
-    //     }
-    // }, 600);
-    
 
 })
