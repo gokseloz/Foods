f9398b4b11762e9832a581da969307c14e43965c
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2021-03-09 14:01:12 +0100

    form validation added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
+// --------------
+// * VARIABLES *
+// --------------
 let foodIcons = [...document.querySelectorAll(".food")]
 let menuBtn = document.querySelector(".menu-btn")
 let nav = document.querySelector(".nav")
 let navLink = document.querySelectorAll(".nav-link")
+let registerForm = document.querySelector(".register-form")
+let registerInput = document.querySelector(".register-input")
+let registerWarningText = document.querySelector(".register-warning")
+let registerSuccessText = document.querySelector(".register-success")
 
+// -----------------------------
+// * INTRO SECTION - ANIMATION *
+// -----------------------------
 i = 0;
 setInterval(() => {
     i++
@@ -18,23 +28,58 @@ setInterval(() => {
     }
 }, 3000);
 
-
+// -----------------------------
+// * NAVBAR and ACCESSIBILITY *
+// -----------------------------
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("is-active")
     nav.classList.toggle("is-active")
 
-    if(menuBtn.classList.contains("is-active")){
+    if (menuBtn.classList.contains("is-active")) {
         menuBtn.setAttribute("aria-expanded", "true")
         menuBtn.setAttribute("aria-pressed", "true")
         navLink.forEach(e => {
-            e.setAttribute("tabindex","0")
+            e.setAttribute("tabindex", "0")
         });
-    }
-    else{
+    } else {
         menuBtn.setAttribute("aria-expanded", "false")
         menuBtn.setAttribute("aria-pressed", "false")
         navLink.forEach(e => {
-            e.setAttribute("tabindex","-1")
+            e.setAttribute("tabindex", "-1")
         });
     }
 })
+
+// --------------------------------------
+// * REGISTER SECTION - FORM VALIDATION *
+// --------------------------------------
+registerInput.addEventListener("invalid", (e) => {
+    e.preventDefault()
+    warning()
+})
+
+registerForm.addEventListener("submit", logSubmit)
+
+function logSubmit(e) {
+    e.preventDefault()
+
+    if (registerInput.value.length == 0) {
+        warning()
+
+    } else {
+        registerSuccessText.style.display = "block"
+        registerInput.value = ""
+
+        setTimeout(() => {
+            registerSuccessText.style.display = "none"
+        }, 3000);
+    }
+}
+
+function warning() {
+    registerWarningText.style.display = "block"
+
+    setTimeout(() => {
+        registerWarningText.style.display = "none"
+    }, 3000);
+}
